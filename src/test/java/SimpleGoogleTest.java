97f0731ccc76bdf9237d9a386d583a29254ee191
roman.girak <roman.girak@gmail.com>
2019-02-12 18:51:51 +0200

    Initial commit

---
+++ src/test/java/SimpleGoogleTest.java
@@ -0,0 +1,28 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SimpleGoogleTest {
+
+    WebDriver drv;
+
+    @Before
+    public void googleTest_setup() {
+        WebDriverManager.chromedriver().setup();
+        drv = new ChromeDriver();
+    }
+
+    @After
+    public void googleTest_cleanup() {
+        drv.quit();
+    }
+
+    @Test
+    public void googleTest() {
+        drv.get("https://www.google.com/");
+        drv.findElement(By.name("q")).sendKeys("Selenium");
+        drv.findElement(By.name("btnK")).click();
+    }
+}
